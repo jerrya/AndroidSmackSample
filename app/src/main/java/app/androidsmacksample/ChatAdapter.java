b019ba275c3c24dc36e2ffea19868476380c327a
Jerry Alex <jerryalex08@gmail.com>
2015-08-05 20:09:54 -0500

    Added ChatAdapter documentation

--- app/src/main/java/app/androidsmacksample/ChatAdapter.java
+++ app/src/main/java/app/androidsmacksample/ChatAdapter.java
@@ -11,17 +11,26 @@ import java.util.List;
 
 public class ChatAdapter extends ArrayAdapter<ChatItem>{
 
+    /*
+    * Viewholder is used for performance improvement. The ListView is populated
+    * sped up by caching previous views.
+    * */
     private static class ViewHolder {
         TextView mChatSender;
         TextView mChatMessage;
     }
 
+    /*
+    * We pass a basic List container for the listview adapter. This can be adapted
+    * to be a few different types of collections.
+    * */
     public ChatAdapter(Context context, List<ChatItem> chatList) {
         super(context, 0, chatList);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        // Retrieve data for this item position
         ChatItem chat = getItem(position);
 
         ViewHolder viewHolder;
